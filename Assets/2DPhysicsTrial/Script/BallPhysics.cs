680d6d21778322b49914dce41d12ad7183b1df98
hsinpa <hsinpa@gmail.com>
2020-03-31 21:03:05 +0800

    no message

---
+++ Assets/2DPhysicsTrial/Script/BallPhysics.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallPhysics : MonoBehaviour
+{
+    private Vector2 gravity = new Vector2(0, -9.8f);
+    private float DeltaTime = 0.02f;
+
+    [SerializeField]
+    private float mass = 0;
+
+    [SerializeField]
+    private Vector2 velocity;
+
+    [SerializeField]
+    private Vector2 force;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        Vector3 combineVelocity = velocity + gravity;
+        transform.position = transform.position + (combineVelocity * DeltaTime);
+    }
+
+}
